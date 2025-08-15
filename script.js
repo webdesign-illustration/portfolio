8015d17664f9c11f589485145a3a387b59c25a23
kaorusakurai-design <info@kaorusakurai.com>
2025-08-15 17:37:11 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -15,4 +15,28 @@ $(function() {
       $(this).find('.text-contents').removeClass('text-active');
     }
   );
+ });
+
+	
+ $(window).on('scroll load', function(){        /* ページロード時、またはスクロールされた時*/
+  var scroll = $(this).scrollTop();            /* 現在のスクロール量を測定 */
+  var windowHeight = $(window).height();       /* ウィンドウの高さを測定 */
+  $('.fadeIn').each(function(){                /* 「fadeIn」のクラスがついているものを1つずつ確認し・・・ */
+    var cntPos = $(this).offset().top;         /* 対象の要素の上からの距離を測定 */
+    if(scroll > cntPos - windowHeight + windowHeight / 3){  /* 要素がある位置までスクロールされていたら */
+      $(this).addClass('active');              /* 「active」のクラスを付与 */
+    }
+  });
+});
+
+$(document).on('ready', function() {
+  $(".full-screen").slick({
+    centerMode: true,
+    centerPadding: '5%',
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 1000,
+    infinite: true,
+  });
 });
